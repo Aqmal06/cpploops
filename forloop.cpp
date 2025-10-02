a25daceb12448cfa08948d826774f1d4d6b718d2
Muhd Aqmal <muhammadaqmal721@gmail.com>
2025-10-02 16:55:58 +0800

    forloop
    Forloop briefing.

---
+++ forloop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+
+//Case 1
+    for(int i=5; i>=1; i--){
+        std::cout<<i<<'\n';
+    }
+    std::cout<<"Happy Birthday!\n";
+//Case 2
+    for(int i=1; i<=3; i+=1){
+        std::cout<<i<<'\n';
+    }
+    std::cout<<"Congratulations!";
+}
